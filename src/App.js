22353bfa43cc044165713aaebe2bb200b96496ad
Elena Tsoy <ltsoy1120@gmail.com>
2023-10-10 09:13:32 +0600

    data transfer to iframe

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     ></iframe>`
       let win = window.frames.biometrics
       const message = token
-      win.postMessage(message, "*")
+      win.postMessage(message, "https://biometrics.paydala.kz/frame/")
     }
   }
   return (
