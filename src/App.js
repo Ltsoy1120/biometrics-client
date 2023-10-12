580a80a965f407c0889232d44e351c1a22c86f7b
Elena Tsoy <ltsoy1120@gmail.com>
2023-10-12 11:22:08 +0600

    added input fields

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import { useState, useEffect } from "react"
 import "./App.css"
 
 function App() {
-  let user = {
-    userId: "user",
+  const [user, setUser] = useState({
+    userId: "",
     clientId: "test",
     clientSecret: "secret"
-  }
-  const [token, setToken] = useState("")
+  })
+  const [state, setState] = useState({
+    userId: "",
+    token: "",
+    iin: ""
+  })
+
+  console.log("user", user)
+  console.log("state", state)
+  console.log("JSON.stringify(state)", JSON.stringify(state))
 
   useEffect(() => {
-    if (token) {
+    if (state.token && state.userId) {
       const frame = document.getElementById("frame")
       frame.style.display = "block"
 
       let win = window.frames.biometrics
-      win.postMessage(token, "*")
+      win.postMessage(JSON.stringify(state), "*")
     }
-  }, [token])
+  }, [state.token, state.userId, state])
 
   const onClickHandler = () => {
     const getToken = async () => {
@@ -32,15 +40,43 @@ function App() {
         }
       )
         .then(response => response.json())
-        .then(data => setToken(data.token))
+        .then(data => setState({ ...state, token: data.token }))
     }
     getToken()
   }
+  const onChangeHandler = e => {
+    const value = e.target.value
+    const name = e.target.name
+    setUser({ ...user, [name]: value })
+    setState({ ...state, [name]: value })
+  }
+
   return (
     <div className="App">
+      <div className="inputs">
+        <div className="input-field">
+          <label htmlFor="userId">Обязательное поле *</label>
+          <input
+            id="userId"
+            name="userId"
+            placeholder="Ваш userId"
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="iin">Необязательное поле</label>
+          <input
+            id="iin"
+            name="iin"
+            placeholder="Ваш ИИН"
+            onChange={onChangeHandler}
+          />
+        </div>
+      </div>
       <button onClick={onClickHandler}>Пополнить</button>
       <iframe
         id="frame"
+        title="Frame"
         name="biometrics"
         src="https://biometrics.paydala.kz/frame/"
         allow="camera"
